341d3cd2f73002b7f318e0c67b8bfd9859ca5572
gouravmarch20 <gouravmarch20@gmail.com>
2021-10-12 00:13:55 +0530

    added : header

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import React from 'react'
 import Home from './pages/Home'
 const App = () => {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,27 @@
 import { useState } from 'react'
-
-const Header = ({ heading, paragraph, children }) => {
+// import { LazyLoadImage } from 'react-lazy-load-image-component'
+import { useHistory } from 'react-router-dom'
+const Header = ({ heading, paragraph, children, image }) => {
   const [state] = useState({
     video: '/assets/videos/header.mp4',
     poster: '/assets/images/screen.png',
     logo: '/assets/images/logo.png'
   })
+//   const { push } = useHistory()
+  const goToHome = () => {
+    // push('/')
+  }
   return (
-    <div>
-      <div className='header'>
-        {/* 
-          <div className="container pr">
-              <div className="header__logo">
-
-              </div>
-          </div>
-      */}
-        <div className='header__video'>
+    <div className='header'>
+      <div className='container pr'>
+        <div className='header__logo'>
+          {/* <LazyLoadImage src={state.logo} alt='logo' onClick={goToHome} /> */}
+        </div>
+      </div>
+      <div className='header__video'>
+        {image ? (
+          {/* <LazyLoadImage src={image} alt={image} /> */}
+        ) : (
           <video
             src={state.video}
             autoPlay
@@ -24,12 +29,22 @@ const Header = ({ heading, paragraph, children }) => {
             muted
             poster={state.poster}
           ></video>
+        )}
+      </div>
+      <div className='header__contents'>
+        <div className='container'>
+          <div className='header__contents__text'>
+            <div className='header__contents__text__child'>
+              <h1 className='header__contents__text__child__h1'>{heading}</h1>
+              <p className='header__contents__text__child__p'>{paragraph}</p>
+              <div className='header__contents__text__child__link'>
+                {children}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      {children}
     </div>
   )
 }
-
 export default Header
