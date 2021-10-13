dd93ed0835c744b7de9a6f1accaab737f2d4b43f
gouravmarch20 <gouravmarch20@gmail.com>
2021-10-13 10:26:17 +0530

    context api setup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import React from 'react'
 import Home from './pages/Home'
+// contexts
+import ModelContext from './context/contexts/ModelContext'
 const App = () => {
+  console.log(ModelContext)
   return (
     <div>
       <Home />
