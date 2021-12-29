88f3ab77b87af56021d0bf91f3fefee729188464
gouravmarch20 <gouravmarch20@gmail.com>
2021-12-29 10:54:15 +0530

    fix:; yarn to npm

--- src/context/providers/ModelProvider.js
+++ src/context/providers/ModelProvider.js
@@ -1,20 +1,19 @@
-import { useState, useReducer } from 'react'
-import ModelContext from '../contexts/ModelContext'
-import ModelReducer from '../reducers/ModelReducer'
-const ModelProvider = props => {
-  const [state, dispatch] = useReducer(ModelReducer, {
-    modelStatus: false,
-    current: ''
-  })
-  return (
-    <ModelContext.Provider
-      value={{
-        state,
-        dispatch
-      }}
-    >
-      {props.children}
-    </ModelContext.Provider>
-  )
-}
-export default ModelProvider
+import { useState, useReducer } from 'react';
+import ModelContext from '../ModelContext';
+import ModelReducer from '../reducers/ModelReducer';
+const ModelProvider = (props) => {
+	const [state, dispatch] = useReducer(ModelReducer, {
+		modelStatus: false,
+		current: '',
+	});
+	return (
+		<ModelContext.Provider
+			value={{
+				state,
+				dispatch,
+			}}>
+			{props.children}
+		</ModelContext.Provider>
+	);
+};
+export default ModelProvider;
